1dce1f6688e7b66b54ec783f36f6742e7936965b
sreethin <sreethin.tblr@gmail.com>
2020-06-06 17:11:01 +0530

    wip

--- dist/app.controller.js
+++ dist/app.controller.js
@@ -10,24 +10,22 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
-const app_service_1 = require("./app.service");
+const microservices_1 = require("@nestjs/microservices");
 let AppController = class AppController {
-    constructor(appService) {
-        this.appService = appService;
-    }
-    getHello() {
-        return this.appService.getHello();
+    constructor() { }
+    async handleMessagePrinted(data) {
+        console.log(data.text);
     }
 };
 __decorate([
-    common_1.Get(),
+    microservices_1.EventPattern('message_printed'),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", []),
-    __metadata("design:returntype", String)
-], AppController.prototype, "getHello", null);
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], AppController.prototype, "handleMessagePrinted", null);
 AppController = __decorate([
     common_1.Controller(),
-    __metadata("design:paramtypes", [app_service_1.AppService])
+    __metadata("design:paramtypes", [])
 ], AppController);
 exports.AppController = AppController;
 //# sourceMappingURL=app.controller.js.map
